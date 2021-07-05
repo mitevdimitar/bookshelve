55b287a0a95d9519fe0de032ceb9ddd722603ba2
Dimitar Mitev <mitev.dimitar@gmail.com>
2021-07-05 08:45:19 +0300

    implement react router

--- src/App/bookshelve_app.js
+++ src/App/bookshelve_app.js
@@ -1,8 +1,39 @@
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
+import Home from "../Pages/home";
+import Login from "../Pages/login";
+
 function App() {
   return (
-    <div className="App">
-      Bookshelve app
-    </div>
+    <Router>
+      <div>
+        <nav>
+          <ul>
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+            <li>
+              <Link to="/login">Login</Link>
+            </li>
+          </ul>
+        </nav>
+
+        {/* A <Switch> looks through its children <Route>s and
+            renders the first one that matches the current URL. */}
+        <Switch>
+          <Route path="/login">
+            <Login />
+          </Route>
+          <Route path="/">
+            <Home />
+          </Route>
+        </Switch>
+      </div>
+    </Router>
   );
 }
 
