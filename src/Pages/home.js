d07e1a6cbce5966ca2b76da48754f1e05710f606
Dimitar Mitev <mitev.dimitar@gmail.com>
2021-07-06 08:56:37 +0300

    home page with background

--- src/Pages/home.js
+++ src/Pages/home.js
@@ -1,10 +1,21 @@
 import React from 'react';
+import Grid from "@material-ui/core/Grid";
+import { makeStyles } from "@material-ui/core/styles";
+import Background from '../img/home_screen.jpg';
+
+const useStyles = makeStyles((theme) => ({
+    root: {
+      height: "100vh",
+      backgroundImage: `url(${Background})`
+    },
+  }));
 
 function Home() {
+    const classes = useStyles();
     return (
-        <div>
+        <Grid container className={classes.root}>
             Home
-        </div>
+        </Grid>
     )
 }
 
