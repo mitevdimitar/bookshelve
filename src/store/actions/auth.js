110d7e5515013d945d99dfbdc6d2ffe7285fc66f
Dimitar Mitev <mitev.dimitar@gmail.com>
2021-07-22 14:35:18 +0300

    signup action

--- src/store/actions/auth.js
+++ src/store/actions/auth.js
@@ -1,10 +1,25 @@
-import { AUTH_SUCCESS, AUTH_ERROR } from "./action_types";
+import { SIGNUP_SUCCESS, SIGNUP_ERROR } from "./action_types";
 import firebase from "../../services/firebase";
+
 // Signing up with Firebase
 export const signup = (email, password) => async dispatch => {
   try {
-    
+    firebase
+      .auth()
+      .createUserWithEmailAndPassword(email, password)
+      .then(data => {
+        console.log(data)
+        dispatch({
+          type: SIGNUP_SUCCESS,
+          payload:
+            "Your account was successfully created! Now you need to verify your e-mail address, please go check your inbox."
+        });
+      })
   } catch (err) {
-    
+    dispatch({
+      type: SIGNUP_ERROR,
+      payload:
+        "Something went wrong, we couldn't create your account. Please try again."
+    });
   }
 };
