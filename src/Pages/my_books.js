0c85e17f39cd7eb75a9c2867854faf37ca30d326
Dimitar Mitev <mitev.dimitar@gmail.com>
2021-08-05 09:31:30 +0300

    add book button

--- src/Pages/my_books.js
+++ src/Pages/my_books.js
@@ -1,10 +1,41 @@
 import React from "react";
+import Grid from "@material-ui/core/Grid";
+import Button from '@material-ui/core/Button';
+import { makeStyles } from '@material-ui/core/styles';
+
+const useStyles = makeStyles((theme) => ({
+    root: {
+      //height: '100vh',
+      marginTop: 20
+    },
+    button: {
+        margin: 20
+    }
+  }));
+  
 
 function MyBooks() {
+    const classes = useStyles();
+
+    const openAddBookModal = () => {
+        console.log('modal opened')
+    }
+
     return(
-        <div>
-            My books
-        </div>
+        <Grid container className={classes.root}>
+            <Grid container item className={classes.button}>
+                <Button
+                    variant="outlined"
+                    color="primary"
+                    onClick={openAddBookModal}
+                >
+                    Add book
+                </Button>
+            </Grid>
+            <Grid>
+                My books
+            </Grid>
+        </Grid>
     )
 }
 
