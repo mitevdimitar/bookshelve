8538ec8ce77e0b01938347f1237dd2f7f87410c0
Dimitar Mitev <mitev.dimitar@gmail.com>
2021-08-06 08:52:11 +0300

    open modal on add book click

--- src/Pages/my_books.js
+++ src/Pages/my_books.js
@@ -2,6 +2,7 @@ import React from "react";
 import Grid from "@material-ui/core/Grid";
 import Button from '@material-ui/core/Button';
 import { makeStyles } from '@material-ui/core/styles';
+import Modal from '@material-ui/core/Modal';
 
 const useStyles = makeStyles((theme) => ({
     root: {
@@ -10,19 +11,46 @@ const useStyles = makeStyles((theme) => ({
     },
     button: {
         margin: 20
-    }
-  }));
+    },
+    paper: {
+        position: 'absolute',
+        width: "50%",
+        height: 500,
+        top: "10%",
+        left: "20%",
+        backgroundColor: theme.palette.background.paper,
+        border: `2px solid ${theme.palette.primary.main}`,
+        borderRadius: 10,
+        boxShadow: theme.shadows[5],
+        padding: theme.spacing(2, 4, 3),
+    },
+}));
   
 
 function MyBooks() {
     const classes = useStyles();
+    const [open, setOpen] = React.useState(false);
 
     const openAddBookModal = () => {
-        console.log('modal opened')
-    }
+        setOpen(true);
+    };
+
+    const handleClose = () => {
+        setOpen(false);
+    };
 
     return(
         <Grid container className={classes.root}>
+            <Modal
+                open={open}
+                onClose={handleClose}
+                aria-labelledby="simple-modal-title"
+                aria-describedby="simple-modal-description"
+            >
+                <div className={classes.paper}>
+                    Modal
+                </div>
+            </Modal>
             <Grid container item className={classes.button}>
                 <Button
                     variant="outlined"
