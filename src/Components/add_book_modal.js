4dacef934762ebcb863f4d90dc1127aa5ef86d88
Dimitar Mitev <mitev.dimitar@gmail.com>
2021-08-06 09:19:56 +0300

    add modal header and close button

--- src/Components/add_book_modal.js
+++ src/Components/add_book_modal.js
@@ -1,5 +1,8 @@
 import React from 'react';
 import Modal from '@material-ui/core/Modal';
+import Grid from "@material-ui/core/Grid";
+import Typography from '@material-ui/core/Typography';
+import HighlightOffIcon from '@material-ui/icons/HighlightOff';
 import { makeStyles } from '@material-ui/core/styles';
 
 const useStyles = makeStyles((theme) => ({
@@ -13,8 +16,17 @@ const useStyles = makeStyles((theme) => ({
         border: `2px solid ${theme.palette.primary.main}`,
         borderRadius: 10,
         boxShadow: theme.shadows[5],
-        padding: theme.spacing(2, 4, 3),
+        //padding: theme.spacing(3),
     },
+    header: {
+        height: 50,
+        backgroundColor: theme.palette.primary.main,
+        color: "white",
+        paddingLeft: 20
+    },
+    closeIcon: {
+        cursor: "pointer"
+    }
 }));
 
 function AddBookModal({
@@ -27,12 +39,27 @@ function AddBookModal({
         <Modal
             open={open}
             onClose={handleClose}
-            aria-labelledby="simple-modal-title"
-            aria-describedby="simple-modal-description"
+            aria-labelledby="Add book modal"
+            aria-describedby="Modal for adding books to database"
         >
-            <div className={classes.paper}>
-                Modal 2
-            </div>
+            <Grid /* container  */className={classes.paper}>
+                <Grid container className={classes.header}>
+                    <Grid container alignItems="center" item xs={11}>
+                        <Typography variant="h6">
+                            Add book
+                        </Typography>
+                    </Grid>
+                    <Grid container alignItems="center" justify="center" item xs={1}>
+                        <HighlightOffIcon className={classes.closeIcon} onClick={()=>handleClose()} />
+                    </Grid>
+                </Grid>
+                <Grid container>
+                    Body
+                </Grid>
+                <Grid container>
+                    Footer
+                </Grid>
+            </Grid>
         </Modal>
     )
 }
