906fd84f773a4e118b1243b409f664e117438ab9
Dimitar Mitev <mitev.dimitar@gmail.com>
2021-09-09 13:41:47 +0300

    book genre constants

---
+++ src/services/constants.js
@@ -0,0 +1,50 @@
+const GENRES = [
+    {
+        name: "Biography",
+        value: "biography"
+    },
+    {
+        name: "Children's",
+        value: "childrens"
+    },
+    {
+        name: "Crime",
+        value: "crime"
+    },
+    {
+        name: "Fantasy",
+        value: "fantasy"
+    },
+    {
+        name: "History",
+        value: "history"
+    },
+    {
+        name: "Horror",
+        value: "horror"
+    },
+    {
+        name: "Mistery",
+        value: "mistery"
+    },
+    {
+        name: "Other",
+        value: "other"
+    },
+    {
+        name: "Poetry",
+        value: "poetry"
+    },
+    {
+        name: "Romance",
+        value: "romance"
+    },
+    {
+        name: "Science",
+        value: "science"
+    },
+    {
+        name: "Thriller",
+        value: "thriller"
+    },
+]
